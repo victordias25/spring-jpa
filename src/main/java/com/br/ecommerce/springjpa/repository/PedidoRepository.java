3edc3647cab6017ba9f7c83ded2782cff464c0de
Victor Dias Nicola <victor.apdias@gmail.com>
2022-09-06 18:13:50 -0300

    Delete src/main directory

--- src/main/java/com/br/ecommerce/springjpa/repository/PedidoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.br.ecommerce.springjpa.repository;
-
-import com.br.ecommerce.springjpa.model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository  extends JpaRepository<Pedido, Long> {
-
-}
